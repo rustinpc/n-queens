a64ca7ca9e53440db8544d865e592b0b6944a78f
rustinpc <rustinpc@gmail.com>
2014-11-06 13:25:38 -0800

    Progress on nRooks

--- src/solvers.js
+++ src/solvers.js
@@ -15,46 +15,95 @@
 
 window.findNRooksSolution = function(n) {
   // handle n === 0 case
-  if (n === 0) {
-    return [];
-  }
-
-  var solution = new Board({'n': n});
+  if (n === 0) {return [];}
+  // create n x n board
+  var solutionBoard = new Board({'n': n});
 
   // iterate through solution board rows
-  for (var i = 0; i < n; i++) {
+  for (var row = 0; row < n; row++) {
     // iterate through row items
-    for (var j = 0; j < n; j++) {
-      // if position is available and there are no conflicts
-      if (solution.rows()[i][j] === 0 && !solution.hasRowConflictAt(j)) {
-        // toggle position
-        solution.togglePiece(i, j);
-        // skip to next row
-        break;
+    for (var col = 0; col < n; col++) {
+      // if position is available...
+      if (solutionBoard.rows()[row][col] === 0) {
+        // ...toggle position
+        solutionBoard.togglePiece(row, col);
+        // if conflicts exist...
+        if (solutionBoard.hasColConflictAt(col)) {
+          // ...untoggle piece
+          solutionBoard.togglePiece(row, col);
+        } else {
+          break;
+        }
       }
     }
   }
-  // return solution board
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution.rows();
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solutionBoard));
+  // return solution board
+  return solutionBoard.rows();
 };
 
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-  var factorial = function(n){
-    if (n === 0) {
-      return 1;
+window.countNRooksSolutions = function(n, board, row, solutions) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  debugger;
+
+  // set up current board, current row, and total solutions counted
+  board = board || new Board({'n': n});
+  row = row || 0;
+  solutions = solutions || 0;
+
+  // iterate through col positions of current row
+  for (var col = 0; col < n; col++) {
+    // check if any col position is toggled
+    if (board.rows()[row][col] === 1) {
+      board.togglePiece(row, col);
+      if (col + 1 === n && row + n === n) {
+        return solutions;
+      } else if (col +  1 === n) {
+        solutions = window.countNRooksSolutions(n, board, row-1, solutions);
+      }
+      col += 1;
     }
-    return n * factorial(n-1);
-  };
+    // toggle cell
+    board.togglePiece(row, col);
+    // if no row or column conflicts exist
+    if (!board.hasAnyRooksConflicts()) {
+      // if next row does not exist
+      if (row + 1 === n) {
+        // increment total solutions
+        debugger;
+        solutions += 1;
+        // check if next col exists
+        if (col + 1 === n) {
+          board.togglePiece(row, col);
+          // recursion
+          solutions = window.countNRooksSolutions(n, board, row-1, solutions);
+        } else {
+          board.togglePiece(row, col);
+          // col += 1;
+        }
+      } else {
+        // else recursively pass in n, board, row+1, and solutions
+        solutions = window.countNRooksSolutions(n, board, row+1, solutions);
+      }
+    } else {
+      // else untoggle cell
+      board.togglePiece(row, col);
+      // check if next col exists
+      if (col + 1 === n) {
+        solutions = window.countNRooksSolutions(n, board, row-1, solutions);
+      }
+    }
+  }
 
-  var solutionCount = factorial(n);
-
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  console.log('Number of solutions for ' + n + ' rooks:', solutions);
+  // return solutions count
+  return solutions;
 };
 
 
@@ -63,7 +112,7 @@ window.countNRooksSolutions = function(n) {
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  // console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
 
@@ -72,6 +121,6 @@ window.findNQueensSolution = function(n) {
 window.countNQueensSolutions = function(n) {
   var solutionCount = undefined; //fixme
 
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+  // console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
